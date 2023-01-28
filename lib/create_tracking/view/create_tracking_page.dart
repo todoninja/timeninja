21fb8599a4b4865051657428ac398e67342ccc4b
Jonas Richard Richter <kontakt@jonasrichter.eu>
2023-01-28 22:44:32 +0100

    Add temporary app icon

--- lib/create_tracking/view/create_tracking_page.dart
+++ lib/create_tracking/view/create_tracking_page.dart
@@ -1,35 +1,94 @@
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' as drift;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timeninja/data/database.dart';
+import 'package:timeninja/keys.dart';
+import 'package:timeninja/l10n/l10n.dart';
 
 class CreateTrackingPage extends StatelessWidget {
   const CreateTrackingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+    final theme = Theme.of(context);
+
     return Provider(
       // The single instance of AppDatabase
       create: (_) => AppDb(),
       builder: (context, child) {
         return Scaffold(
-          appBar: AppBar(title: const Text('Create Tracking')),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () async {
-              final db = Provider.of<AppDb>(context, listen: false);
-
-              await db.into(db.timeblock).insert(
-                    TimeblockCompanion(
-                      start: Value(DateTime.now()),
-                      end: Value(DateTime.now()),
-                      note: const Value('Test'),
-                    ),
-                  );
-            },
-            child: const Icon(Icons.add),
+          appBar: AppBar(
+            title: Text(l10n.navBarHome),
           ),
-          body: Center(
-            child: _buildTimeblockList(context),
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                mini: true,
+                onPressed: () async {
+                  final db = Provider.of<AppDb>(context, listen: false);
+
+                  await db.into(db.timeblock).insert(
+                        TimeblockCompanion(
+                          start: drift.Value(DateTime.now()),
+                          end: drift.Value(DateTime.now()),
+                          note: const drift.Value('Test'),
+                        ),
+                      );
+                },
+                child: const Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              // Padding(
+              //   padding: const EdgeInsets.only(
+              //     left: largeSpacing,
+              //     right: largeSpacing,
+              //   ),
+              //   child: Text(
+              //     "🌤️ Today isn't a work day. Enjoy your free time!",
+              //     textAlign: TextAlign.start,
+              //     style: theme.textTheme.headlineMedium
+              //         ?.copyWith(fontWeight: FontWeight.bold),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(mediumSpacing),
+                child: Card(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text(
+                          '📝 Writing a blog post',
+                        ),
+                        subtitle: Text(
+                          'Started 10:00am',
+                          style: theme.textTheme.labelLarge
+                              ?.copyWith(color: theme.colorScheme.secondary),
+                        ),
+                        trailing: const Text(
+                          '1h 30m',
+                        ),
+                      ),
+                      const LinearProgressIndicator(
+                        value: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(child: _buildTimeblockList(context)),
+            ],
           ),
         );
       },
@@ -56,5 +115,12 @@ StreamBuilder<List<TimeblockData>> _buildTimeblockList(BuildContext context) {
 }
 
 Widget _buildListItem(TimeblockData itemTask, AppDb database) {
-  return Text(itemTask.note);
+  return ListTile(
+    title: Text(itemTask.note),
+    subtitle: Text(itemTask.start.toString() + itemTask.end.toString()),
+    trailing: IconButton(
+      icon: const Icon(Icons.info_outline),
+      onPressed: () {},
+    ),
+  );
 }
